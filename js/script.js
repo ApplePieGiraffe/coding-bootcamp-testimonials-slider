19da9be77ec4f28a555a1d67a47fbd979a52a39e
ApplePieGiraffe <itsmejoshwoodson@gmail.com>
2020-10-28 13:43:14 +0300

    Finish up site

--- js/script.js
+++ js/script.js
@@ -6,22 +6,27 @@ const reviewText = document.querySelector('.review-text');
 const authorName = document.querySelector('.author__name');
 const authorTitle = document.querySelector('.author__title');
 
-btnPrev.addEventListener('click', tada);
-btnNext.addEventListener('click', tada);
-
 let bit = true;
 
-function tada() {
+function changeContent() {
     if (bit) {
-        reviewText.textContent = ' If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ';
+        reviewText.textContent = '“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”';
         authorName.textContent = 'John Tarkpor';
         authorTitle.textContent = 'Junior Frontend Developer';
         image.src = './img/image-john.jpg';
     } else {
-        reviewText.textContent = ' I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ';
+        reviewText.textContent = '“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”';
         authorName.textContent = 'Tanya Sinclair';
         authorTitle.textContent = 'UX Engineer';
         image.src = './img/image-tanya.jpg';
     }
     bit = !bit;
 }
+
+document.addEventListener('keydown', (event) => {
+    if (event.which === 37 || event.which === 39) {
+        changeContent();
+    }
+});
+btnPrev.addEventListener('click', changeContent);
+btnNext.addEventListener('click', changeContent);
